1c03e528a4cb88b1754fa8e60cd134dd9fa746d9
Mustafa-Noorzaiy <Mustafa.Noorzaiy@gmail.com>
2021-03-03 01:24:06 -0800

    Worked with List not HashMap

--- src/main/java/pageObject/SearchBar.java
+++ src/main/java/pageObject/SearchBar.java
@@ -9,39 +9,37 @@ import org.openqa.selenium.WebElement;
 public class SearchBar {
 
 	WebDriver driver;
-	
-	By searchBar=By.id("gh-ac");
-	By searchButton= By.xpath("//input[@id='gh-btn']");
-	
+
+	By searchBar = By.id("gh-ac");
+	By searchButton = By.xpath("//input[@id='gh-btn']");
+
 	By Description = By.xpath("//h3[@class='s-item__title s-item__title--has-tags']");
 	By Price = By.xpath("//span[@class='s-item__price']");
-	
-	public SearchBar(WebDriver driver){
-		this.driver=driver;
+
+	public SearchBar(WebDriver driver) {
+		this.driver = driver;
 	}
-	
+
 	public void searchItem(String item) {
 		driver.findElement(searchBar).sendKeys(item);
 	}
-	
+
 	public void search() {
 		driver.findElement(searchButton).click();
 	}
-	public void getDescription() {
-	
-	HashMap<List<WebElement>, List<WebElement>> hm = new HashMap<List<WebElement>, List<WebElement>>();
 
-	List<WebElement> key=driver.findElements(Description);
-	List<WebElement> value =driver.findElements(Price);
-		
-	for (WebElement we1 : key) {
-		
-		for (WebElement we2 : value) {
-			System.out.println(we1.getText()+ " Price "+we2.getText() );
-			
+	public void getDescription() {
+
+		List<WebElement> key = driver.findElements(Description);
+		List<WebElement> value = driver.findElements(Price);
+
+		for (WebElement we1 : key) {
+
+			for (WebElement we2 : value) {
+				System.out.println(we1.getText() + " Price " + we2.getText());
+
+			}
 		}
-	}
-	
-	
+
 	}
 }
